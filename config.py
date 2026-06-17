575490b6f231925fe007d647df34eeb4ba67d0d9
Patel Het <hetbpatel2906@gmail.com>
2026-06-17 11:54:01 +0530

    initial commit

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+basedir=os.path.abspath(os.path.dirname(__file__))
+
+class Config():
+    SECRET_KEY='mykey'
+    SQLALCHEMY_DATABASE_URI='sqlite:///'+ os.path.join(basedir, 'course_management.db')
+    SQLALCHEMY_TACK_MODIFICATIONS= False
+    
